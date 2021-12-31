ee4f1a5ba7edde80329d7105fc2f45171ac9b287
mastiaha <mastiaha@mail.ru>
2021-12-31 22:18:12 +0300

    Добавьте файлы проекта.

---
+++ Задача 8.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задача_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string put = @"C:\BIM моделирование\Модуль 1\Visual Studio\8 Laba";
+            if (Directory.Exists(put))
+            {
+                Console.WriteLine("Папки");
+                string [] papki = Directory.GetDirectories(put);
+                foreach (string s in papki)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы");
+                string[] faili = Directory.GetFiles(put);
+                foreach (string s in faili)
+                {
+                    Console.WriteLine("\t{0}",s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 8.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задача_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs2/Log.txt";
+            StreamWriter sw = new StreamWriter(path);
+            Random random = new Random();
+            sw.WriteLine(random.Next(0, 10));
+            sw.Close();
+            
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+        }
+    }
+}
